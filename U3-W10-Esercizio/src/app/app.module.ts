122e40f3d4408ea4ed1ca8100afcf770c45b2ba7
EarthDragon10 <lagudadev@gmail.com>
2022-10-31 17:02:00 +0100

    add main components

--- U3-W10-Esercizio/src/app/app.module.ts
+++ U3-W10-Esercizio/src/app/app.module.ts
@@ -9,9 +9,11 @@ import { HeaderSearchBarComponent } from './header-search-bar/header-search-bar.
 import { HeaderNavbarBrandComponent } from './header-navbar-brand/header-navbar-brand.component';
 import { HeaderNavbarCollapseComponent } from './header-navbar-collapse/header-navbar-collapse.component';
 import { HeaderItemDropdownComponent } from './header-item-dropdown/header-item-dropdown.component';
+import { MainCardsComponent } from './main-cards/main-cards.component';
+import { MainCardComponent } from './main-card/main-card.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, MainComponent, FooterComponent, HeaderSearchBarComponent, HeaderNavbarBrandComponent, HeaderNavbarCollapseComponent, HeaderItemDropdownComponent],
+  declarations: [AppComponent, HeaderComponent, MainComponent, FooterComponent, HeaderSearchBarComponent, HeaderNavbarBrandComponent, HeaderNavbarCollapseComponent, HeaderItemDropdownComponent, MainCardsComponent, MainCardComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
